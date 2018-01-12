97a5ddf2cfc35d8b352bec7c77b1931defa668f7
Robert James Patterson <guajiropa@hotmail.com>
2018-01-11 21:29:45 -0800

    Updated comic book detail view to be strongly typed

--- ComicBookGallery/Models/Artist.cs
+++ ComicBookGallery/Models/Artist.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace ComicBookGallery.Models
 {
-    public class Artists
+    public class Artist
     {
         public int Id { get; set; }
         public string Name { get; set; }
