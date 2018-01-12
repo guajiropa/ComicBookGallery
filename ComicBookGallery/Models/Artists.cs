b1753b1b8f306b378f6de7c4c9011a86cfed9c65
Robert James Patterson <guajiropa@hotmail.com>
2018-01-11 20:12:45 -0800

    Added comic book and artists models

---
+++ ComicBookGallery/Models/Artists.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class Artists
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
